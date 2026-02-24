f9921e68803261240679f23c856d52af96b08379
ecanales23 <eycanal@mit.edu>
2026-02-23 19:11:51 -0500

    Step 3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h1>Emily Canales</h1>
+<nav> 
+    <a class="current" href=".">Home</a>
+    <a href="projects">Projects</a>
+    <a href="contact">Contact</a>
+    <a href="resume">Resume</a>
+    <a href="https://github.com/ecanales23" target="_blank">GitHub</a>
+</nav> 
+<p>Hi there! My name is Emily Canales. I am a senior at MIT in Course 11 (Urban Planning). </p>
+<img src = "images/linkedin photo.jpg" alt = "Picture of myself">
+
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,27 @@
 <svelte:head>
   <title>Contact me</title>
 </svelte:head>
+<h1> Contact Me</h1>
+<nav> 
+    <a href=".">Home</a>
+    <a href="projects">Projects</a>
+    <a href="contact" class="current">Contact</a>
+    <a href="resume">Resume</a>
+    <a href="https://github.com/ecanales23" target="_blank">GitHub</a>
+</nav>
+    <form action="mailto:canalesemily33@gmail.com" method="get" enctype="text/plain">
+        <label>
+            Email:
+            <input name="email" type="email" value="" >
+        </label>
+        <label>
+            Subject:
+            <input name="subject" value="">
+        </label>
+        <label>
+             Message:
+            <textarea name="body"></textarea> 
+        </label>
+
+        <button>Send</button>
+    </form>
